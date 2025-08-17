5f82732ec5fd1d5f97ef0bddf8d761b8f46b57b8
invitacionesweb <invitacionesweb.info@gmail.com>
2025-08-17 12:44:02 -0300

    AC
    AC

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Establecer la fecha de destino (puedes ajustarla según tus necesidades)
-const fechaDestino = new Date("2025-11-08T23:59:59");
+const fechaDestino = new Date("2025-09-26T23:59:59");
 
 function actualizarContador() {
   const ahora = new Date();
